9671c1e1b6f8858cc9bd443d7ebd07a2957d5284
lg519 <lg519@ic.ac.uk>
2023-06-07 22:30:06 +0100

    server changes

--- back-end/server.py
+++ back-end/server.py
@@ -1,37 +1,37 @@
-from flask import Flask, request, make_response
+from flask import Flask, request
+from flask_cors import CORS
 
 from werkzeug.utils import secure_filename
 import os, pdb
 import tensorflow as tf
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 app.debug = True
 
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    return response
-
 @app.route("/file", methods=["POST"])
 def upload_keras():
-    #pdb.set_trace()
     if "file" not in request.files:
         return "No file part", 400
     file = request.files["file"]
-    filename = secure_filename(file.filename)
-    filepath = os.path.join("/tmp", filename)
+    filename = file.filename
+    print(f"Received filename: {filename}")
+    filepath = os.path.join("my_model.pb", filename)
+    #filepath = "my_model.pb"
+    print(f"Saving file to: {filepath}")
     file.save(filepath)
 
-    # Load the TensorFlow .pb model
-    model = tf.keras.models.load_model(filepath) # my_model.pb (directory)
-    os.remove(filepath)  # delete the uploaded file
+    print(f"model saved to: {filepath}")
 
-    # Get the model layers
-    layer_names=[layer.name for layer in model.layers]
+    model = tf.keras.models.load_model("my_model.pb")
 
-    # Here you can define the layers supported by your hardware accelerators
+    print(f"model loaded")
+    os.remove(filepath)
+
+    layer_names = [layer.name for layer in model.layers]
+    print(layer_names)
+
     accelerator_1_layers = ["conv2d", "max_pooling2d"]
     accelerator_2_layers = ["conv2d", "max_pooling2d", "batch_normalization"]
     accelerator_3_layers = ["conv2d", "max_pooling2d", "batch_normalization"]
@@ -44,9 +44,9 @@ def upload_keras():
     if all(layer in layer_names for layer in accelerator_3_layers):
         compatible_accelerators.append(3)
 
-    return add_cors_headers(make_response({"compatibleAccelerators": compatible_accelerators, "layers": layers}))
-
+    compatible_accelerators = []
+    compatible_accelerators.append(1)
+    return {"compatibleAccelerators": compatible_accelerators}
 
 if __name__ == "__main__":
-    #upload_keras("/home/username/Desktop/ef_model/EF_hackaton/EF_hackaton/my_model.pb")
     app.run(port=8000)
