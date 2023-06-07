060509508370d64a7dcc4b56def8b324bda40e57
lg519 <lg519@ic.ac.uk>
2023-06-07 23:17:11 +0100

    working solution

--- back-end/server.py
+++ back-end/server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+import zipfile
 from werkzeug.utils import secure_filename
 import os, pdb
 import tensorflow as tf
+import shutil
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -12,29 +13,43 @@ app.debug = True
 
 @app.route("/file", methods=["POST"])
 def upload_keras():
+    # Check and create directory if it doesn't exist
+    if not os.path.exists('uploaded_model'):
+        os.makedirs('uploaded_model')
+
     if "file" not in request.files:
         return "No file part", 400
     file = request.files["file"]
     filename = file.filename
     print(f"Received filename: {filename}")
+
+    # Save the file
     filepath = os.path.join("uploaded_model", filename)
-    #filepath = "my_model.pb"
     print(f"Saving file to: {filepath}")
     file.save(filepath)
 
-    print(f"model saved to: {filepath}")
+    # Extract the zip file
+    with zipfile.ZipFile(filepath, 'r') as zip_ref:
+        zip_ref.extractall("uploaded_model")
 
-    model = tf.keras.models.load_model("uploaded_model")
+    # Print the contents of the directory "uploaded_model"
+    print("Contents of 'uploaded_model':")
+    print(os.listdir("uploaded_model"))
+
+    # Assume the model is in a directory with the same name as the original filename (minus .zip)
+    model_dir = filename.rstrip(".zip")
+    model_filepath = os.path.join("uploaded_model", model_dir)
+
+    model = tf.keras.models.load_model(model_filepath)
 
     print(f"model loaded")
-    os.remove(filepath)
 
     layer_names = [layer.name for layer in model.layers]
     print(layer_names)
 
     accelerator_1_layers = ["conv2d", "max_pooling2d"]
     accelerator_2_layers = ['conv2d', 'batch_normalization', 'max_pooling2d', 'conv2d_1', 'batch_normalization_1', 'max_pooling2d_1', 'flatten', 'dense']
-    accelerator_3_layers = ["conv2d", "max_pooling2d", "batch_normalization"]
+    accelerator_3_layers = ["conv2d", "max_pooling2d", "batch_normalization", "bob"]
 
     compatible_accelerators = []
     if all(layer in layer_names for layer in accelerator_1_layers):
@@ -44,8 +59,13 @@ def upload_keras():
     if all(layer in layer_names for layer in accelerator_3_layers):
         compatible_accelerators.append(3)
 
-    compatible_accelerators = []
-    compatible_accelerators.append(1)
+    # Remove the zip file
+    os.remove(filepath)
+
+    # Clean up the directory
+    shutil.rmtree('uploaded_model')
+
+    print(f"Compatible accelerators: {compatible_accelerators}")
     return {"compatibleAccelerators": compatible_accelerators}
 
 if __name__ == "__main__":
